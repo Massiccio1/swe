118298387ca888eb9124d7a68c7896e3aa4554e5
Massiccio1 <girardellimassimo@gmail.com>
2023-06-06 09:37:00 +0200

    jest tesing 3

--- app/tests/students.test.js
+++ app/tests/students.test.js
@@ -4,28 +4,44 @@
 const request = require('supertest');
 const jwt     = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const app     = require('../app');
+const Student = require('../models/student'); // get our mongoose model
+const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
+
 
 describe('GET /api/v1/students/me', () => {
 
   // Moking User.findOne method
   let userSpy;
+  let connection;
+  let db;
 
-  beforeAll( () => {
-    const User = require('../models/student');
-    userSpy = jest.spyOn(User, 'findOne').mockImplementation((criterias) => {
-      return {
-        id: 1212,
-        email: 'John@mail.com',
-        password: ''
-      };
+  beforeAll( async () => {
+    // const User = require('../models/student');
+    // userSpy = jest.spyOn(User, 'findOne').mockImplementation((criterias) => {
+    //   return {
+    //     id: 1212,
+    //     email: 'John@mail.com',
+    //     password: ''
+    //   };
+    // });
+    console.log("connecting to: ",process.env.DB_URL);
+    app.locals.db = mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then ( () => {
+        
+        console.log("Connected to Database");
+       
     });
   });
 
   afterAll(async () => {
     userSpy.mockRestore();
+    await connection.close();
   });
   
   test('GET /api/v1/students/me with no token should return 401', async () => {
+    let students = await Student.find({email: "e1@gmail.com"});
+    console.log("students found: ",students);
     const response = await request(app).get('/api/v1/students/me');
     expect(response.statusCode).toBe(401);
   });
