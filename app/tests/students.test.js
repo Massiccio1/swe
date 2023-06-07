ccdf89a920eaa2bd56bbc5cef71db0ab7ce9487b
Massiccio1 <girardellimassimo@gmail.com>
2023-06-07 16:20:50 +0200

    merge fix

--- app/tests/students.test.js
+++ app/tests/students.test.js
@@ -5,16 +5,6 @@ const request = require('supertest');
 const jwt     = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const app     = require('../app');
 const Student = require('../models/student'); // get our mongoose model
-<<<<<<< HEAD
-
-=======
-const {MongoClient} = require('mongodb');
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-
-
->>>>>>> 9aba9c946bcc47fdaeb834ca5cfd954cbcf2059b
 describe('GET /api/v1/students/me', async () => {
 
   // Moking User.findOne method
@@ -68,33 +58,6 @@ describe('GET /api/v1/students/me', async () => {
     const response = await request(app).get('/api/v1/students/me?token=123456');
     expect(response.statusCode).toBe(403);
   });
-<<<<<<< HEAD
-
-  
-      
-  test('GET /api/v1/students/me?token=<valid> should return 200', async () => {
-    //expect.assertions(1);
-    console.log(Student);
-    let user = await Student.findOne({
-      email: "e1@gmail.com"
-    });
-  
-    let account_type = "student";
-    
-    // if user is found and password is right create a token
-    var payload = {
-      email: user.email,
-      id: user._id,
-      type: account_type
-      // other data encrypted in the token	
-    }
-  
-    var options = {
-      expiresIn: 86400 // expires in 24 hours
-    }
-    var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
-    const response = await request(app).get('/api/v1/students/me?token='+token);
-=======
   
   let students = await Student.find({email: "e1@gmail.com"});
     console.log("students found: ",students);
@@ -115,40 +78,11 @@ describe('GET /api/v1/students/me', async () => {
       
   test('GET /api/v1/students/me?token=<valid> should return 200', async () => {
     const response = await request(app).get('/api/v1/students/me?token=' + token);
->>>>>>> 9aba9c946bcc47fdaeb834ca5cfd954cbcf2059b
     expect(response.statusCode).toBe(200);
     //done();
   });
 
   test('GET /api/v1/students/me?token=<valid> should return user information', async () => {
-<<<<<<< HEAD
-    //expect.assertions(2);
-    let user = await Student.findOne({
-      email: "e1@gmail.com"
-    });
-  
-  
-    let account_type = "student";
-    
-    // if user is found and password is right create a token
-    var payload = {
-      email: user.email,
-      id: user._id,
-      type: account_type
-      // other data encrypted in the token	
-    }
-  
-    var options = {
-      expiresIn: 86400 // expires in 24 hours
-    }
-    var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
-    const response = await request(app).get('/api/v1/students/me?token='+token);
-    const user_body = response.body;
-    expect(user_body).toBeDefined();
-    expect(user_body.email).toBe('e1@mail.com');
-    //done();
-    //done();
-=======
     
     const response = await request(app).get('/api/v1/students/me?token='+token);
     const user = response.body
@@ -156,6 +90,5 @@ describe('GET /api/v1/students/me', async () => {
     .expect(user.email).toBe('e1@mail.com');
     console.log("recived response body: ", user);
 
->>>>>>> 9aba9c946bcc47fdaeb834ca5cfd954cbcf2059b
   });
 });
