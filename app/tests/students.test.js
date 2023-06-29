62636c3939aa40b460e1ae5121eeb222ebeafc81
Massiccio1 <girardellimassimo@gmail.com>
2023-06-29 10:40:24 +0200

    test 1000

--- app/tests/students.test.js
+++ app/tests/students.test.js
@@ -6,7 +6,7 @@ const jwt     = require('jsonwebtoken'); // used to create, sign, and verify tok
 const app     = require('../app');
 const Student = require('../models/student'); // get our mongoose model
 const mongoose = require('mongoose');
-describe('GET /api/v1/students/me', async () => {
+describe('GET /api/v1/students/me', () => {
 
   // Moking User.findOne method
   let userSpy;
@@ -23,9 +23,13 @@ describe('GET /api/v1/students/me', async () => {
     //   };
     // });
     await mongoose.connection.close();
+    console.log("mongo: ", process.env.DB_URL);
     await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then ( () => {
-      console.log("Connected to Database")
+      console.log("Connected to Database");
+
     });
+    let students = await Student.find({email: "e1@gmail.com"});
+    console.log("students found 1: ",students);
   });
 
   afterAll(async () => {
@@ -44,30 +48,48 @@ describe('GET /api/v1/students/me', async () => {
     expect(response.statusCode).toBe(403);
   });
   
-  let students = await Student.find({email: "e1@gmail.com"});
-    console.log("students found: ",students);
-  // create a valid token
-  let account_type = "student";
-	
-	// if user is found and password is right create a token
-	var payload = {
-		email: students.email,
-		id: students._id,
-		type: account_type
-		// other data encrypted in the token	
-	}
-	var options = {
-		expiresIn: 86400 // expires in 24 hours
-	}
-	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+
       
   test('GET /api/v1/students/me?token=<valid> should return 200', async () => {
+    let students = await Student.find({email: "e1@gmail.com"});
+    console.log("students found: ",students);
+    // create a valid token
+    let account_type = "student";
+    
+    // if user is found and password is right create a token
+    var payload = {
+      email: students.email,
+      id: students._id,
+      type: account_type
+      // other data encrypted in the token	
+    }
+    var options = {
+      expiresIn: 86400 // expires in 24 hours
+    }
+    var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
+    
     const response = await request(app).get('/api/v1/students/me?token=' + token);
     expect(response.statusCode).toBe(200);
     //done();
   });
 
   test('GET /api/v1/students/me?token=<valid> should return user information', async () => {
+    let students = await Student.find({email: "e1@gmail.com"});
+    console.log("students found: ",students);
+    // create a valid token
+    let account_type = "student";
+    
+    // if user is found and password is right create a token
+    var payload = {
+      email: students.email,
+      id: students._id,
+      type: account_type
+      // other data encrypted in the token	
+    }
+    var options = {
+      expiresIn: 86400 // expires in 24 hours
+    }
+    var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
     
     const response = await request(app).get('/api/v1/students/me?token='+token);
     const user = response.body
