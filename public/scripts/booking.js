c5d9d31ea5e1687d4929a7b98ec6acb4e2895414
fip20 <filippo.maffei-2@studenti.unitn.it>
2023-07-09 14:29:55 +0200

    loadBookings

--- public/scripts/booking.js
+++ public/scripts/booking.js
@@ -1,10 +1,24 @@
 function loadBookings() {
+
+const cookies = document.cookie.split("; "); // Ottieni tutti i cookie come array
+let token = null;
+
+for (let i = 0; i < cookies.length; i++) {
+  const cookie = cookies[i].split("=");
+  if (cookie[0] === "token") {
+    token = cookie[1];
+    break;
+  }
+}
     var prenotations = document.getElementById("bookings");
     const url = "/api/v1/prenotations";
   
-    fetch(url,{
-      'x-access-token': loggedUser.token,
-      'X-User-Id': loggedUser.id
+    fetch('/api/v1/prenotations', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
     })
       .then((resp) => resp.json())
       .then((prenotazioni) => {
@@ -18,6 +32,6 @@ function loadBookings() {
         console.log(error);
       });
   }
-  
+
   loadBookings();
   
